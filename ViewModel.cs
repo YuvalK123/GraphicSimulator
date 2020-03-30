883f22392d5bcc2c369e8e9f5698cff2e5fe0bcd
israelameg <59085159+israelameg@users.noreply.github.com>
2020-03-30 16:59:14 +0300

    Add files via upload

---
+++ ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace ex1
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        private ISimulatorModel model;
+        
+        public ViewModel(ISimulatorModel model)
+        {
+            this.model = model;
+            model.PropertyChanged += delegate(Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("VM" + e.PropertyName);
+                
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+
+    }
+}
